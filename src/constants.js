686a29b708a73e4b08689b8b14803af18e87b161
Aman Holkar <amanholkar00@gmail.com>
2025-02-11 08:29:09 +0530

    implement the auth with sign-up method still not implement sign-in logout

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const DB_NAME = "TodoDatabase";
 
-/**
- * @type {{ADMIN: "ADMIN"; USER: "USER" as const}}
- */
+// /**
+//  * @type {{ADMIN: "ADMIN"; USER: "USER" as const}}
+//  */
 export const UserRolesEnum = {
     ADMIN: "ADMIN",
     USER: "USER",
